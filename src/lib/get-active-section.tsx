3ed679a591b4a92b20b67a451ec9b4c695bb4c06
sajjathossain <sajjathossain.official@gmail.com>
2024-08-29 17:36:05 +0600

    chore(get-active-section): add new method

--- src/lib/get-active-section.tsx
+++ src/lib/get-active-section.tsx
@@ -30,3 +30,15 @@ export const useActiveSection = () => {
     };
   });
 };
+
+export const getActiveSection = ({ elements }: { elements: NodeListOf<Element> }) => {
+  for (const ref of elements) {
+    const isInView = ScrollTrigger.isInViewport(ref);
+
+    if (isInView) {
+      const activeId = ref.getAttribute('data-active-id') as TActiveSection;
+      activeSectionAtom.set(activeId);
+    }
+  }
+
+};
