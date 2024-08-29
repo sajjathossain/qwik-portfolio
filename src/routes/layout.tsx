6eca18d1e1244986e86625ea63186751fce1bb93
sajjathossain <sajjathossain.official@gmail.com>
2024-08-29 13:20:01 +0600

    chore(routes/layout): disable cache

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,17 +2,17 @@ import { component$, Slot } from '@builder.io/qwik';
 import type { RequestHandler } from '@builder.io/qwik-city';
 import { Cursor, PageScrollDistance } from '~/components';
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
+export const onGet: RequestHandler = async (/* { cacheControl } */) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
-  if (process.env.NODE_ENV === 'production') {
+  /* if (process.env.NODE_ENV === 'production') {
     cacheControl({
       // Always serve a cached response by default, up to an hour stale
       staleWhileRevalidate: 60 * 60,
       // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
       maxAge: 5
     });
-  }
+  } */
 };
 
 export default component$(() => {
