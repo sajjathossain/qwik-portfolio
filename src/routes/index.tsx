4bb0991da9c156a311e53918d03d5a8e7a44f00f
sajjathossain <sajjathossain.official@gmail.com>
2024-08-29 21:49:03 +0600

    chore(routes): render qwik skills

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { AboutSection } from '~/components/sections';
 import { ExperiencesSection } from '~/components/sections/experiences';
+import { QwikSkills } from '~/integrations/react';
 
 export default component$(() => {
   return (
     <>
       <AboutSection />
       <ExperiencesSection />
+      <QwikSkills />
     </>
   );
 });
