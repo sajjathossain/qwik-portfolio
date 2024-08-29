03272762879e29d699a996314f5d4523af1f674d
sajjathossain <sajjathossain.official@gmail.com>
2024-08-29 15:55:21 +0600

    feat(routes/index): add experiences section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { AboutSection } from '~/components/sections/about';
+import { AboutSection } from '~/components/sections';
+import { ExperiencesSection } from '~/components/sections/experiences';
 
 export default component$(() => {
   return (
     <>
       <AboutSection />
+      <ExperiencesSection />
     </>
   );
 });
