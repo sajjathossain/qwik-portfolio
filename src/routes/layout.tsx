e37b569aafd499876baf0740cbadbe8ae179741a
sajjathossain <sajjathossain.official@gmail.com>
2024-08-29 23:46:34 +0600

    chore(layout): enable cache on production

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,17 +4,17 @@ import { Cursor, PageScrollDistance } from '~/components';
 import { SideNavbar } from '~/integrations/react';
 import { QwikTopNavbar } from '~/integrations/react/top-navbar';
 
-export const onGet: RequestHandler = async (/* { cacheControl } */) => {
+export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
-  /* if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     cacheControl({
       // Always serve a cached response by default, up to an hour stale
       staleWhileRevalidate: 60 * 60,
       // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
       maxAge: 5
     });
-  } */
+  }
 };
 
 export default component$(() => {
