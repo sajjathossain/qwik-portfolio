184cfab7a27fb0c1cc433cb15bbd890874abaf0c
sajjathossain <sajjathossain.official@gmail.com>
2024-08-30 16:13:24 +0600

    chore(layouts): use named layout instead of wrapping them

--- src/routes/blogs/index.tsx
+++
@@ -1,9 +0,0 @@
-import LayoutBlogs from './layout.blogs';
-
-export default function Index() {
-  return (
-    <LayoutBlogs>
-      <div>This is blogs page</div>
-    </LayoutBlogs>
-  );
-}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,13 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 import { AboutSection } from '~/components/sections';
 import { ExperiencesSection } from '~/components/sections/experiences';
 import { QwikSkills } from '~/integrations/react';
-import LayoutRoot from './layout.root';
 
 export default component$(() => {
   return (
     <>
-      <LayoutRoot>
-        <AboutSection />
-        <ExperiencesSection />
-        <QwikSkills />
-      </LayoutRoot>
+      <AboutSection />
+      <ExperiencesSection />
+      <QwikSkills />
     </>
   );
 });
