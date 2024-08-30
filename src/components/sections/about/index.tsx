9a35a90871cca5fe8d7946b33de424b338acac80
sajjathossain <sajjathossain.official@gmail.com>
2024-08-30 09:57:01 +0600

    fix(about): fix style

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -22,8 +22,8 @@ export const AboutSection = component$(() => {
     <Container>
       <Section id="about">
         <div class="my-auto flex h-full w-full flex-col-reverse items-center justify-center gap-12 md:flex-row">
-          <div class="relative grid w-full place-items-center  md:h-full">
-            <div class="grow-cursor mix-blend-darkenabsolute mt-2 flex w-full flex-col items-center justify-between gap-2 md:z-10 md:mt-0 md:items-start md:gap-0">
+          <div class="grid w-full place-items-center md:relative  md:h-full">
+            <div class="grow-cursor flex w-full flex-col items-center justify-between gap-2 md:absolute md:z-10  md:items-start md:gap-0">
               <QwikTypewriter />
               <div class="mt-2 flex w-full flex-col items-center gap-1 pr-2 text-center text-sm md:items-start md:pr-0 md:text-start md:text-base">
                 {descriptions.map((description) => (
@@ -33,14 +33,14 @@ export const AboutSection = component$(() => {
                 ))}
               </div>
             </div>
-            <GridAnimation classes={cn('relative hidden md:block md:ml-20')} />
+            <GridAnimation classes={cn('relative hidden md:flex md:ml-20')} />
           </div>
           <img
             src={profilePicture}
             alt="Profile picture"
             width={200}
             height={200}
-            class="aspect-square w-52 rounded-xl outline outline-2 outline-offset-4 outline-slate-700 md:z-10 md:w-64 md:rounded-full "
+            class="z-0 aspect-square w-52 rounded-xl outline outline-2 outline-offset-4 outline-slate-700 md:z-10 md:w-64 md:rounded-full "
           />
         </div>
       </Section>
