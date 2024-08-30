fe5014d92a1f9b36b0ac8a9fb099d0c7ba65e1d5
sajjathossain <sajjathossain.official@gmail.com>
2024-08-30 20:49:54 +0600

    chore(blogs): show all available blogs

--- src/routes/blogs/index@blog.tsx
+++ src/routes/blogs/index@blog.tsx
@@ -1,3 +1,70 @@
-export default function Index() {
-  return <div>This is blogs page</div>;
-}
+import { component$ } from '@builder.io/qwik';
+import { readdirSync, readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { blogsDir } from './[slug]/index@markdown';
+import matter from 'gray-matter';
+import { Container } from '~/components/container';
+import type { TBreadcrumbList } from '~/integrations/react';
+import { QwikBreadcrumb } from '~/integrations/react';
+import { BlogCard } from '~/components/blog/card';
+import { frontmatterSchema } from '~/lib/types';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+export const useLoadData = routeLoader$(async () => {
+  const blogPaths = readdirSync(join(blogsDir));
+
+  const blogs = blogPaths.map((p) => {
+    const buffer = readFileSync(join(blogsDir, p));
+    const fileContent = matter(buffer);
+    const parsed = frontmatterSchema.parse(fileContent.data);
+    return {
+      ...parsed,
+      slug: p.replace('.md', '').replace(' ', '-').toLowerCase()
+    };
+  });
+
+  return blogs;
+});
+
+export default component$(() => {
+  const data = useLoadData();
+  const blogs = data.value;
+  const breadcrumbList: TBreadcrumbList = [
+    {
+      title: 'Blogs',
+      href: '/blogs'
+    }
+  ];
+
+  const sortedBlogs = blogs.sort((a, b) => {
+    const isGreater =
+      new Date(b.pubDate).valueOf() - new Date(a.pubDate).valueOf();
+    return isGreater > 0 ? 1 : -1;
+  });
+
+  return (
+    <Container classes="py-6 h-dvh w-full px-2 md:px-0 grid grid-rows-[100% 1fr]">
+      <div>
+        <div class="mb-3 flex w-full items-center justify-between">
+          <h1 class="text-center text-3xl md:text-left">Blogs</h1>
+          <div class="flex items-center gap-4">
+            <p class="text-center text-xs text-gray-400 md:text-left md:text-sm">
+              {blogs.length} blogs
+            </p>
+          </div>
+        </div>
+        <QwikBreadcrumb classes="my-2" list={breadcrumbList} />
+        <hr class="bg-emerald-500/35 my-2 h-[2px] md:my-4" />
+        <div data-pagefind-ui></div>
+      </div>
+      <div
+        data-blog-container
+        class="grid max-h-full grid-cols-1 gap-4 overflow-y-auto no-scrollbar"
+      >
+        {sortedBlogs.map((blog) => {
+          return <BlogCard key={blog.slug} blog={blog} />;
+        })}
+      </div>
+    </Container>
+  );
+});
