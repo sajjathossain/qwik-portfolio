0c4280fc9ae514271dad573f686ebaa64a7813cd
sajjathossain <sajjathossain.official@gmail.com>
2024-08-31 09:54:53 +0600

    chore(blog/card): mod styles

--- src/components/blog/card.tsx
+++ src/components/blog/card.tsx
@@ -16,30 +16,30 @@ export const BlogCard = component$((props: Props) => {
 
   const link = `/blogs/${slug}`;
   return (
-    <article class="h-fit w-full rounded-md bg-slate-800 outline outline-2 outline-emerald-500/25">
+    <article class="h-full w-full overflow-hidden rounded-md bg-slate-800">
       <OptimizedImage
         layout="fixed"
         width={1000}
         height={420}
-        classes="rounded-t-sm"
         src={`/assets/blogs/${image}?jsx`}
         alt={slug}
       />
-      <div class="grid h-fit gap-3 px-3 py-4 md:gap-2">
-        <div class="grid gap-1 md:gap-2">
-          <Link href={link}>
-            <h1 class="text-xl">{title}</h1>
-          </Link>
+      <div class="grid h-fit w-full gap-3 px-3 py-4 md:gap-2">
+        <div class="grid gap-2">
+          <h1 class="truncate whitespace-nowrap text-lg md:text-xl">{title}</h1>
           {/* {description && <p class="card-description">{description}</p>} */}
 
-          <div class="flex flex-col flex-wrap items-start justify-between gap-3 text-sm text-gray-400 md:flex-row md:items-center md:gap-0">
-            <time dateTime={new Date(pubDate).toISOString()}>
+          <div class="grid w-full grid-cols-[auto_1fr] items-center text-sm text-gray-400 md:items-center md:gap-0">
+            <time
+              class="flex w-fit flex-shrink"
+              dateTime={new Date(pubDate).toISOString()}
+            >
               {new Date(pubDate).toLocaleDateString(undefined, {
                 dateStyle: 'medium',
                 timeZone: 'UTC'
               })}
             </time>
-            <div class="flex items-center gap-1 text-xs md:text-sm">
+            <div class="flex items-center justify-end gap-1 text-xs md:text-sm">
               {tags?.map((item: string) => (
                 <p key={item} class="rounded-sm bg-blue-900 p-1 text-gray-300">
                   {item}
@@ -48,12 +48,12 @@ export const BlogCard = component$((props: Props) => {
             </div>
           </div>
         </div>
-        <a
-          href={`/blogs/${slug}`}
-          class="hidden w-fit rounded-sm bg-blue-500 p-1 text-sm text-white md:block md:p-2"
+        <Link
+          href={link}
+          class="w-full rounded-sm bg-blue-500 p-2 text-center text-sm text-white md:w-fit"
         >
           Read more
-        </a>
+        </Link>
       </div>
     </article>
   );
